12887f18d74d2ed21949ea269dff2f6f2c820521
Juneroy Quinimon <eroyjune@gmail.com>
2024-05-13 03:25:29 +0800

    feat: added new controller for path finding controller a*

---
+++ app/Http/Controllers/PathFindingController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\AStarService;
+use Illuminate\Http\Request;
+
+class PathFindingController extends Controller
+{
+    //
+    public function findPath(Request $request)
+    {
+        $start = $request->input('start', 'A');
+        $goal = $request->input('goal', 'D');
+        $graph = [
+            'A' => ['B' => 10.0, 'C' => 2.5],
+            'B' => ['A' => 10.0, 'D' => 1.0],
+            'C' => ['A' => 2.5, 'D' => 1.5],
+            'D' => ['B' => 1.0, 'C' => 1.5],
+        ];
+        // $positions = [
+        //     'A' => [40.7128, -74.0060],
+        //     'B' => [41.8781, -87.6298],
+        //     'C' => [34.0522, -118.2437],
+        //     'D' => [29.7604, -95.3698],
+        // ];
+        $positions = [
+            'A' => [0, 0],
+            'B' => [1, 0],
+            'C' => [0, 1],
+            'D' => [1, 1],
+        ];
+        
+        $astar = new AStarService($graph, $positions);
+        $path = $astar->findPath($start, $goal);
+
+        return response()->json(['path' => $path]);
+    }
+}
