057e11eb0b0f27bedb9dfb6016fa170bf430557d
Juneroy Quinimon <eroyjune@gmail.com>
2024-05-12 21:04:09 +0800

    feat: added new service for a* algorithm

---
+++ app/Services/AStarService.php
@@ -0,0 +1,82 @@
+<?php
+namespace App\Services;
+
+use SplPriorityQueue;
+
+class AStarService
+{
+    private $positions;
+    private $graph;
+
+    public function __construct($graph, $positions)
+    {
+        $this->graph = $graph;
+        $this->positions = $positions;
+    }
+
+    public function findPath($start, $goal)
+    {
+        $openSet = new SplPriorityQueue();
+        $openSet->insert($start, 0);
+        $cameFrom = [];
+        $gScore = [$start => 0];
+        $fScore = [$start => $this->heuristic($start, $goal)];
+        $closedSet = [];
+        
+        while (!$openSet->isEmpty()) {
+            $current = $openSet->extract();
+            if ($current === $goal) {
+                return $this->reconstructPath($cameFrom, $current);
+            }
+            $closedSet[$current] = true;
+
+            foreach ($this->graph[$current] ?? [] as $neighbor => $cost) {
+                if (!empty($closedSet[$neighbor])) {
+                    continue;
+                }
+                $tentativeGScore = floatval($gScore[$current]) + floatval($cost);
+                // echo "$tentativeGScore";
+                echo "$neighbor - $gScore[$current] '-' $cost\n";
+                // echo "$cost\n";
+                if (!isset($gScore[$neighbor]) || $tentativeGScore < $gScore[$neighbor]) {
+                    $cameFrom[$neighbor] = $current;
+                    $gScore[$neighbor] = $tentativeGScore;
+                    $fScore[$neighbor] = $tentativeGScore + $this->heuristic($neighbor, $goal);
+                    $openSet->insert($neighbor, -$fScore[$neighbor]);
+                }
+            }
+        }
+        
+
+        return null;
+    }
+
+    private function reconstructPath($cameFrom, $current)
+    {
+        $path = [];
+        while (isset($cameFrom[$current])) {
+            $path[] = $current;
+            $current = $cameFrom[$current];
+        }
+        $path[] = $current;
+        return array_reverse($path);
+    }
+
+    private function heuristic($node1, $node2)
+    {
+        $lat1 = deg2rad($this->positions[$node1][0]);
+        $lon1 = deg2rad($this->positions[$node1][1]);
+        $lat2 = deg2rad($this->positions[$node2][0]);
+        $lon2 = deg2rad($this->positions[$node2][1]);
+
+        $dLat = $lat2 - $lat1;
+        $dLon = $lon2 - $lon1;
+
+        $a = sin($dLat / 2) ** 2 + cos($lat1) * cos($lat2) * sin($dLon / 2) ** 2;
+        $c = 2 * atan2(sqrt($a), sqrt(1 - $a));
+        return 6371 * $c; // Earth's radius in kilometers
+    }
+}
+
+
+?>
